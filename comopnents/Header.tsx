0a8555ba5f13bb4035ba1d37a7659bf32b189bdf
Kedmar20 <m.kedzierski76@gmail.com>
2023-05-27 18:10:40 +0200

    change

--- comopnents/Header.tsx
+++ comopnents/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
    return (
       <header>
          <div className="relative cursor-pointer h-10 w-5 opacity-75 transition hover:opacity-100">
-            <Image src="/next.svg" width={384} height={75} alt="logo of nextjs" />
+            <Image src="/next.svg" width={100} height={1000} alt="logo of nextjs" />
             using NextJS to build an e-commerce website
          </div>
       </header>
