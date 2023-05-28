c4dc88500e3e6cc8c21cf4044dbd5b752eda4295
Kedmar20 <m.kedzierski76@gmail.com>
2023-05-28 20:34:16 +0200

    update setuping

--- comopnents/Header.tsx
+++ comopnents/Header.tsx
@@ -4,21 +4,22 @@ import Link from "next/link";
 
 function Header() {
    return (
-      // <header>
-      // {/* <div className="flex items-center justify-center md:w-1/5 ">
-      //    <Link href="/"> */}
-      // {/* <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
-      //          <Image src="/next.svg" width="100" height="10" alt="logo of nextjs" />
-      //       </div> */}
-      // {/* </Link>
-      // </div> */}
-      <div className="flex space-x-2">
-         <a>Product</a>
-         <a>Explore</a>
-         <a>Support</a>
-         <a>Business</a>
-      </div>
-      // </header>
+      <header>
+         <div className=" flex items-center justify-center md:w-1/5 ">
+            <Link href="/">
+               <div className="relative h-1 w-10 cursor-pointer opacity-75 transition hover:opacity-100">
+                  <Image src="/next.svg" width="100" height="10" alt="logo of nextjs" />
+               </div>
+            </Link>
+         </div>
+         {/* <div className="hidden flex-1 items-center justify-center space-x-2 md:flex"> */}
+         <div className="hidden md:flex">
+            <a className="headerLink">Product</a>
+            <a className="headerLink">Explore</a>
+            <a className="headerLink">Support</a>
+            <a className="headerLink">Business</a>
+         </div>
+      </header>
    );
 }
 
