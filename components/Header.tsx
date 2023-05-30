8e3a7d058ea66bbdab7b0f24d9d8a495d8e1b6c0
Kedmar20 <m.kedzierski76@gmail.com>
2023-05-30 12:09:49 +0200

    change inhalt of the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,28 +2,29 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { MagnifyingGlassIcon, ShoppingBagIcon, UserIcon } from "@heroicons/react/24/outline";
-import logoNext from "../public/next.svg";
+// import logoNext from "../public/next.svg";
+import logoGustus from "../public/logo-gustus.svg";
 
 function Header() {
    return (
-      <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-slate-400 p-10">
+      <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-white p-10">
          <div className=" flex items-center justify-center md:w-1/5 ">
             <Link href="/">
-               <div className="relative h-10 w-50 cursor-pointer opacity-75 transition hover:opacity-100 bg-amber-500 ">
-                  <Image src={logoNext} alt="logo of nextjs" width={100} />
+               <div className="relative h-150 w-300 cursor-pointer opacity-75 transition hover:opacity-100">
+                  <Image src={logoGustus} alt="logo of nextjs" width={120} />
                </div>
             </Link>
          </div>
          <div className="hidden flex-1 items-center justify-center space-x-8 md:flex">
-            <a className="headerLink">Product</a>
-            <a className="headerLink">Explore</a>
-            <a className="headerLink">Support</a>
-            <a className="headerLink">Business</a>
+            <a className="headerLink">Frauenuhren</a>
+            <a className="headerLink">Herrenuhren</a>
+            <a className="headerLink">Smartwatch</a>
+            <a className="headerLink">Ausverkauf</a>
          </div>
          <div className="flex items-center justify-center gap-x-4 md:w-1/5">
             <MagnifyingGlassIcon className="headerIcon" />
             <div className="relative cursor-pointer">
-               <span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white">
+               <span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-[#aa0000] to-[#3F0101] text-[10px] text-white">
                   5
                </span>
                <ShoppingBagIcon className="headerIcon" />
