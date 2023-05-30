f0a1767e9d311c903ed2d197aa767b702885b661
Kedmar20 <m.kedzierski76@gmail.com>
2023-05-30 21:18:34 +0200

    add user icon and start session conditioning

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import { MagnifyingGlassIcon, ShoppingBagIcon, UserIcon } from "@heroicons/react
 import logoGustus from "../public/logo-gustus.svg";
 
 function Header() {
+   const session = false;
    return (
       <header className="sticky top-0 z-30 flex w-full items-center justify-between bg-white p-10">
          <div className=" flex items-center justify-center md:w-1/5 ">
@@ -23,12 +24,32 @@ function Header() {
          </div>
          <div className="flex items-center justify-center gap-x-4 md:w-1/5">
             <MagnifyingGlassIcon className="headerIcon" />
-            <div className="relative cursor-pointer">
-               <span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-[#aa0000] to-[#3F0101] text-[10px] text-white">
-                  5
-               </span>
-               <ShoppingBagIcon className="headerIcon" />
-            </div>
+            <Link href="/checkout">
+               <div className="relative cursor-pointer">
+                  <span className="absolute -right-1 -top-1 z-50 flex h-4 w-4 items-center justify-center rounded-full bg-gradient-to-r from-[#aa0000] to-[#3F0101] text-[10px] text-white">
+                     5
+                  </span>
+                  <ShoppingBagIcon className="headerIcon" />
+               </div>
+            </Link>
+            {session ? (
+               <Image
+                  src={
+                     // session.user?.image ||
+                     "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
+                  }
+                  alt=""
+                  className="cursor-pointer rounded-full"
+                  width={34}
+                  height={34}
+                  // onClick={() => signOut()}
+               />
+            ) : (
+               <UserIcon
+                  className="headerIcon"
+                  //  onClick={() => signIn()}
+               />
+            )}
          </div>
       </header>
    );
