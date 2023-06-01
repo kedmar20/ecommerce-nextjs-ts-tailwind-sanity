aad47fc8fb5f87ed2388ca02ad5340933349bfc5
Kedmar20 <m.kedzierski76@gmail.com>
2023-06-01 12:42:54 +0200

    add section2 + add package HeadlessUI for Tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter, Poppins } from "next/font/google";
 import Head from "next/head";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import { Tab } from "@headlessui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({ weight: "300", subsets: ["latin"] });
@@ -16,60 +17,45 @@ export default function Home() {
          </Head>
 
          <Header />
-         <main className={`flex min-h-screen flex-col items-center justify-between p-6 ${poppins.className}`}>
+         <main className={`relative h-[200vh] ${poppins.className}`}>
             {/* <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}> */}
-            <Hero></Hero>
-
-            {/* <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-               <a
-                  href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                  className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                  target="_blank"
-                  rel="noopener noreferrer"
-               >
-                  <h2 className={`mb-3 text-2xl font-semibold`}>
-                     Docs <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                  </h2>
-                  <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Find in-depth information about Next.js features and API.</p>
-               </a>
-
-               <a
-                  href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                  className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                  target="_blank"
-                  rel="noopener noreferrer"
-               >
-                  <h2 className={`mb-3 text-2xl font-semibold`}>
-                     Learn <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                  </h2>
-                  <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-               </a>
-
-               <a
-                  href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                  className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                  target="_blank"
-                  rel="noopener noreferrer"
-               >
-                  <h2 className={`mb-3 text-2xl font-semibold`}>
-                     Templates <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                  </h2>
-                  <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Discover and deploy boilerplate example Next.js&nbsp;projects.</p>
-               </a>
-
-               <a
-                  href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                  className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                  target="_blank"
-                  rel="noopener noreferrer"
-               >
-                  <h2 className={`mb-3 text-2xl font-semibold`}>
-                     Deploy <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                  </h2>
-                  <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-               </a>
-            </div> */}
+            <Hero />
          </main>
+         <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#160303]">
+            <div className="space-y-10 py-16">
+               <h1 className="text-center">SECTION 2</h1>
+               <Tab.Group>
+                  <Tab.List className="flex justify-center">
+                     {/* {categories.map((category) => (
+                        <Tab
+                           key={category._id}
+                           id={category._id}
+                           className={({ selected }) =>
+                              `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                                 selected ? "borderGradient bg-[#35383C] text-white" : "border-b-2 border-[#35383C] text-[#747474]"
+                              }`
+                           }
+                        >
+                           {category.title}
+                        </Tab>
+                     ))} */}
+                     <Tab className="whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 borderGradient bg-[#35383C] text-white border-b-2 ">
+                        Category Title
+                     </Tab>
+                  </Tab.List>
+                  <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
+                     {/* <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+                     <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+                     <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+                     <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel> */}
+                     <Tab.Panel className="tabPanel">showProducts 0</Tab.Panel>
+                     <Tab.Panel className="tabPanel">showProducts 1</Tab.Panel>
+                     <Tab.Panel className="tabPanel">showProducts 2</Tab.Panel>
+                     <Tab.Panel className="tabPanel">showProducts 3</Tab.Panel>
+                  </Tab.Panels>
+               </Tab.Group>
+            </div>
+         </section>
       </>
    );
 }
